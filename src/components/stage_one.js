6b5b4e871d50b9df0c88d1a6c1bfc1194ed6801c
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-02-25 13:28:55 -0600

    React Context

---
+++ src/components/stage_one.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const StageOne = () =>{
+    return(
+        <View>
+          <Text>Stage One</Text>
+        </View>
+    )
+}
+
+export default StageOne;
---
+++ src/components/stage_two.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const StageTwo = () =>{
+    return(
+        <View>
+          <Text>Stage Two</Text>
+        </View>
+    )
+}
+
+export default StageTwo;
